18fdeb625466c36e1ed98c074827a0f1eda5bb49
ri-izdo <me@riizdo.com>
2024-01-22 13:55:08 -0800

    Add 3d objects.

--- MetalFusion/MetalFusion/ContentView.swift
+++ MetalFusion/MetalFusion/ContentView.swift
@@ -27,17 +27,26 @@ struct ContentView: View {
     @State private var isToggled = false
     @State private var isShoeButtonOn = false
     @State private var isAwardMetalButtonOn = false
+    @State private var isMonsterraButtonOn = false
     @State private var isModel3DViewOn = false
-    let shoeButtonString = "3D Shoe"
-    let awardMetalButton = "Award Metal"
+    @State private var assetPath = ""
+    @State private var isShuttlecockButtonOn = false
+    let shoeButtonString = "Shoe"
+    let awardMetalButton = "Gold"
+    let monsterraButtonString = "Plant"
+    let shuttlecockString = "Shuttlecock"
+    let shuttlecockAssetPath = "Assets.scnassets/Shuttlecock/Shuttlecock.gltf"
+    let monsteraAssetPath = "Assets.scnassets/Monsterra/monsterra.gltf"
     let shoeAssetPath = "Assets.scnassets/panoramas/shoe.gltf"
+    let awardAssetPath = "Assets.scnassets/panoramas/Gold.gltf"
+    let backgroundImage = "Assets.scnassets/panoramas/studio_small_09_4k.exr"
 
     var body: some View {
         VStack {
             buttons()
         }
         if isModel3DViewOn {
-            model3DView(assetPath: shoeAssetPath)
+            model3DView(assetPath: assetPath)
                 .frame(maxWidth: 400, maxHeight: 500)
         } else {
             Rectangle()
@@ -49,14 +58,53 @@ struct ContentView: View {
     
     func buttons() -> some View {
         HStack {
+            Button(shuttlecockString) {
+                if !isShuttlecockButtonOn {
+                    isShuttlecockButtonOn.toggle()
+                    isModel3DViewOn = true
+                    isMonsterraButtonOn = false
+                    isShoeButtonOn = false
+                    isAwardMetalButtonOn = false
+                    assetPath = shuttlecockAssetPath
+                }
+            }
+            .buttonStyle(UniversalButtonStyle(isActive: isAwardMetalButtonOn))
+            
+            Button(monsterraButtonString) {
+                if !isMonsterraButtonOn {
+                    isMonsterraButtonOn.toggle()
+                    isModel3DViewOn = true
+                    isShoeButtonOn = false
+                    isAwardMetalButtonOn = false
+                    isShuttlecockButtonOn = false
+                    assetPath = monsteraAssetPath
+                }
+            }
+            .buttonStyle(UniversalButtonStyle(isActive: isAwardMetalButtonOn))
+            
             Button(shoeButtonString) {
-                isShoeButtonOn.toggle()
-                isModel3DViewOn.toggle()
+                if !isShoeButtonOn{
+                    isShoeButtonOn.toggle()
+                    assetPath = shoeAssetPath
+                    isModel3DViewOn = true
+                    isAwardMetalButtonOn = false
+                    isMonsterraButtonOn = false
+                    isShuttlecockButtonOn = false
+                }
             }
             .buttonStyle(UniversalButtonStyle(isActive: isShoeButtonOn))
-//            BUTTON(awardMetalButton) {
-//
-//            }
+            
+            Button(awardMetalButton) {
+                if !isAwardMetalButtonOn {
+                    isAwardMetalButtonOn.toggle()
+                    assetPath = awardAssetPath
+                    isModel3DViewOn = true
+                    isShoeButtonOn = false
+                    isMonsterraButtonOn = false
+                    isShuttlecockButtonOn = false
+                }
+            }
+            .buttonStyle(UniversalButtonStyle(isActive: isAwardMetalButtonOn))
         }
     }
     
@@ -64,6 +112,9 @@ struct ContentView: View {
         Model3DView(named: assetPath)
             .transform(scale: 0.9)
             .cameraControls(OrbitControls(camera: $camera,sensitivity: 0.5))
-            .background(.background)
+            .background(Color.black)
+            .ibl(named: backgroundImage, intensity: 0.4)
+            
+        
     }
 }
